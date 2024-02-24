0ac16af87ebe4f927f75ef77cdce198f8ef6d2ba
Infixo <swidwins@gmail.com>
2024-02-24 21:07:53 +0100

    Dynamic new households limiter, fixes #11

--- Patches/HouseholdSpawn.cs
+++ Patches/HouseholdSpawn.cs
@@ -1,3 +1,4 @@
+using System;
 using HarmonyLib;
 using RealPop.Systems;
 
@@ -7,6 +8,7 @@ namespace RealPop.Patches;
 class HouseholdSpawnSystemPatches
 {
     private static CountHomesSystem m_CountHomesSystem;
+    private static int s_SpawnCounter = 0;
 
     [HarmonyPatch(typeof(Game.Simulation.HouseholdSpawnSystem), "OnUpdate")]
     [HarmonyPrefix]
@@ -20,15 +22,38 @@ class HouseholdSpawnSystemPatches
         if (m_CountHomesSystem == null)
         {
             m_CountHomesSystem = __instance.World.GetOrCreateSystemManaged<CountHomesSystem>();
-            //Plugin.Log("GET SYSTEM");
+            //Plugin.Log($"New households limiter: FreeRatioTreshold={Plugin.FreeRatioTreshold.Value}, FreeRatioFullSpeed={Plugin.FreeRatioFullSpeed.Value}.");
         }
 
-        bool isAbove = m_CountHomesSystem.totalProperties > 0 ? ( 1000 * m_CountHomesSystem.freeProperties / m_CountHomesSystem.totalProperties > Plugin.FreeRatioTreshold.Value ) : false;
-        
-        //Plugin.Log($"IsAbove {isAbove}: total {m_CountHomesSystem.totalProperties} free {m_CountHomesSystem.freeProperties} ratio {1000 * m_CountHomesSystem.freeProperties / m_CountHomesSystem.totalProperties} tres {Plugin.FreeRatioTreshold.Value} ");
-        return isAbove; // true - the job will run, false - no new households
-    }
+        // wait for the CountHomesSystem to activate
+        if (m_CountHomesSystem.totalProperties < 0)
+        {
+            //Plugin.Log($"NewCims: households not counted"); // debug
+            return false;
+        }
 
+        // enable the feature only after 100 properties are in the city
+        if (m_CountHomesSystem.totalProperties < 100)
+        {
+            //Plugin.Log($"NewCims: households <100"); // debug
+            return true;
+        }
+
+        int cappedRatio = Math.Clamp(1000 * m_CountHomesSystem.freeProperties / m_CountHomesSystem.totalProperties, Plugin.FreeRatioTreshold.Value, Plugin.FreeRatioFullSpeed.Value);
+
+        //int tempCounter = s_SpawnCounter;
+
+        s_SpawnCounter += Plugin.FreeRatioTreshold.Value;
+        s_SpawnCounter -= cappedRatio;
+
+        bool activate = s_SpawnCounter <= 0;
+
+        if (activate) s_SpawnCounter += Plugin.FreeRatioFullSpeed.Value;
+
+        //Plugin.Log($"NewCims: households {m_CountHomesSystem.freeProperties}/{m_CountHomesSystem.totalProperties} ratio {cappedRatio} counter {tempCounter} -> {s_SpawnCounter} run {activate}");
+        return activate; // true - the job will run, false - no new households
+    }
+    /*
     [HarmonyPatch(typeof(Game.Simulation.HouseholdSpawnSystem), "GetUpdateInterval")]
     [HarmonyPrefix]
     static bool HouseholdSpawnSystem_GetUpdateInterval(ref int __result)
@@ -36,4 +61,5 @@ class HouseholdSpawnSystemPatches
         __result = 64;
         return false;
     }
+    */
 }
